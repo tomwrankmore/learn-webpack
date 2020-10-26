c0a6daa865799b2ba0a8e7b579d8f8882871b6e6
Tom <tomwrankmore@gmail.com>
2020-10-26 16:24:59 +0000

    added hashing to main.js, then added HTMLplugin so the correct hashed js file would be in the code. It uses template.html.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require ("path");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main[contentHash].js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/template.html"
+  })],
   module: {
     rules: [
       {
